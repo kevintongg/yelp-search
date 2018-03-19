4e1dfdbe059ae89268c759166677d7152e9f2792
Kevin Tong <kcr1230@hotmail.com>
2018-03-19 02:29:12 -0700

    Consolidate project

--- src/cli.js
+++ src/cli.js
@@ -4,11 +4,21 @@ const yargs = require('yargs');
 const flags = yargs.usage('$0: Usage <cmd> [options]')
   .command({
     command: 'search',
-    desc: 'Searches Yelp',
     alias: 's',
+    desc: 'searches for businesses',
+    builder: yargs => yargs.options('l', {
+      alias: 'location',
+      describe: 'area to search',
+    }).option('c', {
+      alias: 'category',
+      describe: 'type of food',
+    }).option('t', {
+      alias: 'term',
+      describe: 'what kind of food you want to look up',
+    }),
     handler: (argv) => {
-    
+      app.search(argv.location, argv.category, argv.term);
     },
   })
-  .help()
+  .help('help')
   .argv;
