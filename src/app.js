5e8ae42afbf118f45449665640cc804a7628809b
Unknown <ly.c.christopher@gmail.com>
2018-03-22 15:54:51 -0700

    added inquirer for categories

--- src/app.js
+++ src/app.js
@@ -2,18 +2,48 @@ const yelp = require('./index');
 const inquirer = require('inquirer');
 
 function search(location, category, term, number) {
-  yelp.search(location, category, term, number).then((item) => {
-    const json = JSON.parse(item);
-    const business = [];
-    json.businesses.forEach((item) => {
-      business.push({
-        id: `${item.id}`,
-        name: `${item.name}`,
-        location: `${item.location.address1}, ${item.location.city}, ${item.location.state} ${item.location.zip_code}`,
+  console.log('start:', category)
+  if(category == true){
+    const choices = ['Resturaunt', 'Bars', 'Food', 'Delivery', 'Takeout']
+    return inquirer.prompt([{
+      type: 'list',
+      message: 'select a CATEGORY to get the three most recent reviews!',
+      name: 'categories',
+      choices,
+    }])
+    .then((answer) =>{
+      category = answer.categories
+    })
+    .then(() => {
+      yelp.search(location, category, term, number)
+      .then((item) => {
+        const json = JSON.parse(item);
+        const business = [];
+        json.businesses.forEach((item) => {
+          business.push({
+            id: `${item.id}`,
+            name: `${item.name}`,
+            location: `${item.location.address1}, ${item.location.city}, ${item.location.state} ${item.location.zip_code}`,
+          });
+        });
+        getReviews(business);
       });
+    })
+  } else {
+    yelp.search(location, category, term, number)
+    .then((item) => {
+      const json = JSON.parse(item);
+      const business = [];
+      json.businesses.forEach((item) => {
+        business.push({
+          id: `${item.id}`,
+          name: `${item.name}`,
+          location: `${item.location.address1}, ${item.location.city}, ${item.location.state} ${item.location.zip_code}`,
+        });
+      });
+      getReviews(business);
     });
-    getReviews(business);
-  });
+  }
 }
 
 function getReviews(business) {
