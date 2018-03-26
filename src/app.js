63bdc61cb039d78406e1c7fb67fb1fd4fdbd3359
thatboytitz <mmejia1337@gmail.com>
2018-03-25 18:43:16 -0700

    Review function clarity
    made reviews take only one business
    changed review layout

--- src/app.js
+++ src/app.js
@@ -2,99 +2,99 @@ const yelp = require('./index');
 const inquirer = require('inquirer');
 
 function search(location, category, term, number, radius, open_now) {
-  if (open_now === 't') {
-    open_now = true;
-  } else if (open_now === 'true') {
-    open_now = true;
+  if(open_now === 't'){
+    open_now = true
+  }else if(open_now ==='true'){
+    open_now = true
   }
-  else if (open_now === 'f') {
-    open_now = false;
-  } else {
-    open_now = false;
+  else if(open_now === 'f'){
+    open_now = false
+  }else{
+    open_now = false
   }
   if (term === true) {
     yelp.list()
-      .then((result) => {
-        const choices = [];
-        const categories = JSON.parse(result);
-        categories.forEach((category) => {
-          choices.push(category.title);
-        });
-        return inquirer.prompt([{
-          type: 'list',
-          message: 'list of category',
-          name: 'categories',
-          choices,
-        }]);
-      })
+    .then((result) => {
+      const choices = [];
+      const categories = JSON.parse(result);
+      categories.forEach((category) => {
+        choices.push(category.title);
+      });
+      return inquirer.prompt([{
+        type: 'list',
+        message: 'list of category',
+        name: 'categories',
+        choices,
+      }])
+    })
       .then((answer) => {
         term = answer.categories;
       })
       .then(() => {
         yelp.search(location, category, term, number, radius, open_now)
           .then((item) => {
-
+            
             const json = JSON.parse(item);
-            console.log('Total searches found: ', json.total);
+            console.log('Total searches found: ', json.total)
             //console.log(json.total)
-            if (json.total < 1) {
-              console.log('No businesses found!');
+            if(json.total < 1){
+              console.log('No businesses found!')
             }
-            else {
+            else{
               const business = [];
               json.businesses.forEach((item) => {
-                let distance_miles = Math.ceil(item.distance / 1609.34);
+                let distance_miles = Math.ceil(item.distance/1609.34)
                 business.push({
                   id: `${item.id}`,
                   name: `${item.name}`,
                   price: `${item.price}`,
-                  distance: `${distance_miles}`,
+                  distance:`${distance_miles}`,
                   location: `${item.location.address1}, ${item.location.city}, ${item.location.state} ${item.location.zip_code}`,
-                });
+                  });
               });
               getReviews(business);
             }
           });
       });
-  } else {
-    yelp.search(location, category, term, number, radius, open_now)
-      .then((item) => {
-        const json = JSON.parse(item);
-        console.log('Total searches found: ', json.total);
-        if (json.total < 1) {
-          console.log('No businesses found!');
-        }
-        else {
-          const business = [];
-          json.businesses.forEach((item) => {
-            //console.log(item)
-            let distance_miles = Math.ceil(item.distance / 1609.34);
-            business.push({
-              id: `${item.id}`,
-              name: `${item.name}`,
-              price: `${item.price}`,
-              distance: `${distance_miles}`,
-              location: `${item.location.address1}, ${item.location.city}, ${item.location.state} ${item.location.zip_code}`,
-            });
+  }else{
+  yelp.search(location, category, term, number, radius, open_now)
+    .then((item) => {
+      const json = JSON.parse(item);
+      console.log('Total searches found: ', json.total)
+      if(json.total < 1){
+        console.log('No businesses found!')
+      }
+      else{
+        const business = [];
+        json.businesses.forEach((item) => {
+          //console.log(item)
+          let distance_miles = Math.ceil(item.distance/1609.34)
+          business.push({
+            id: `${item.id}`,
+            name: `${item.name}`,
+            price: `${item.price}`,
+            distance:`${distance_miles}`,
+            location: `${item.location.address1}, ${item.location.city}, ${item.location.state} ${item.location.zip_code}`,
           });
-          getReviews(business);
-        }
-      });
+        });
+        getReviews(business);
+      }
+    });
   }
 }
 
 function getReviews(businesses) {
   const choices = [];
   businesses.forEach((item) => {
-    if (item.price !== undefined || item.distance !== undefined) {
+    if (item.price !== undefined) {
       choices.push({
         name: `${item.name}: ${item.location}, Price: ${item.price}, About ${item.distance} mile(s) away.`,
-        value: `${item.id}`,
+        value: `${item.id}`
       });
     } else {
       choices.push({
-        name: `${item.name}: ${item.location}`,
-        value: `${item.id}`,
+        name: `${item.name}: ${item.location}, About ${item.distance} mile(s) away.`,
+        value: `${item.id}`
       });
     }
   });
@@ -102,24 +102,22 @@ function getReviews(businesses) {
     type: 'list',
     message: 'select one business to get the three most recent reviews!',
     name: 'business',
-    choices,
+    choices
   }])
     .then((answer) => {
-      reviews(businesses, answer);
+      reviews(businesses.find(business => business.id === answer.business));
     });
 }
 
-function reviews(businesses, answer) {
-  const business = businesses.find(business => business.id === answer.business);
-
+function reviews(business) {
   console.log(`Name: ${business.name}`);
   console.log(`Location: ${business.location}`);
   if (business.price !== undefined) {
     console.log(`Price: ${business.price}\n`);
-  } else {
-    console.log();
-  }
+  } 
+
   console.log('Reviews:');
+  console.log('--------------------------------------------------');
   yelp.reviews(business.id).then((result) => {
     const json = JSON.parse(result);
     json.reviews.forEach((review) => {
